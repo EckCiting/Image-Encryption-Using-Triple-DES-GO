c7a244758c1d980a91b3c4dfea3f910daa20650a
EckCiting <eckciting@outlook.com>
2023-03-27 00:21:37 +0100

    Implement Triple DES

--- internal/crypto.go
+++ internal/crypto.go
@@ -31,3 +31,48 @@ func DesDecryption(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
 
 	return plaintext, nil
 }
+
+func TDESEncryption(data []byte, keys []byte, iv []byte) ([]byte, error) {
+	k1 := keys[:8]
+	encrypted1, err := DesEncryption(data, k1, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	k2 := keys[8:16]
+	decrypted2, err := DesDecryption(encrypted1, k2, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	k3 := keys[16:]
+	encrypted3, err := DesEncryption(decrypted2, k3, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	return encrypted3, err
+}
+
+func TDESDecryption(data []byte, keys []byte, iv []byte) ([]byte, error) {
+
+	k3 := keys[16:]
+	decrypted3, err := DesDecryption(data, k3, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	k2 := keys[8:16]
+	encrypted2, err := DesEncryption(decrypted3, k2, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	k1 := keys[:8]
+	decrypted1, err := DesDecryption(encrypted2, k1, iv)
+	if err != nil {
+		panic(err)
+	}
+
+	return decrypted1, err
+}
--- test/crypto_test.go
+++ test/crypto_test.go
@@ -32,3 +32,29 @@ func TestDESEncryptionDecryption(t *testing.T) {
 		t.Errorf("解密后的结果与原始明文不一致: got %v, want %v", result, plaintext)
 	}
 }
+
+func TestTDESEncryptionDecryption(t *testing.T) {
+	keys := []byte("123456789012345678901234")
+	plaintext, err := image.ReadImage("../pkg/testdata/Genshin_Impact.jpg")
+	if err != nil {
+		t.Fatalf("读取文件发生错误: %v", err)
+	}
+	iv := []byte("00000000")
+
+	// 加密
+	ciphertext, err := internal.TDESEncryption(plaintext, keys, iv)
+	if err != nil {
+		t.Fatalf("加密过程中发生错误: %v", err)
+	}
+
+	// 解密
+	result, err := internal.TDESDecryption(ciphertext, keys, iv)
+	if err != nil {
+		t.Fatalf("解密过程中发生错误: %v", err)
+	}
+
+	// 检查结果是否正确
+	if !bytes.Equal(result, plaintext) {
+		t.Errorf("解密后的结果与原始明文不一致: got %v, want %v", result, plaintext)
+	}
+}
