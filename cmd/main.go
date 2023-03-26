2232f45c41bd8fabafc3b498e7285e63e1d0f953
EckCiting <eckciting@outlook.com>
2023-03-26 00:04:56 +0000

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"Image-Encryption-Using-Triple-DES-GO/internal"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world!")
+	fmt.Println(descore.Test())
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Image-Encryption-Using-Triple-DES-GO
+
+go 1.20
