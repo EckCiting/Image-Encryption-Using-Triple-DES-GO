307803ad7ee06672fde773a60c0bf4a60632daef
EckCiting <eckciting@outlook.com>
2023-03-31 12:23:14 +0100

    Replace crypto/des to manual des

--- internal/crypto.go
+++ internal/crypto.go
@@ -1,34 +1,30 @@
 package internal
 
-import (
-	"crypto/cipher"
-	"crypto/des"
-	"fmt"
-)
-
 func DesEncryption(data []byte, key []byte, iv []byte) ([]byte, error) {
-	block, err := des.NewCipher(key)
-	if err != nil {
-		return nil, fmt.Errorf("error in creating cipher: %v", err)
-	}
+	//block, err := des.NewCipher(key)
+	//if err != nil {
+	//	return nil, fmt.Errorf("error in creating cipher: %v", err)
+	//}
+	//
+	//ciphertext := make([]byte, len(data))
+	//blockMode := cipher.NewCBCEncrypter(block, iv)
+	//blockMode.CryptBlocks(ciphertext, data)
 
-	ciphertext := make([]byte, len(data))
-	blockMode := cipher.NewCBCEncrypter(block, iv)
-	blockMode.CryptBlocks(ciphertext, data)
-
+	ciphertext := Cipher(data, key, ENCRYPT_MODE)
 	return ciphertext, nil
 }
 
 func DesDecryption(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
-	block, err := des.NewCipher(key)
-	if err != nil {
-		return nil, fmt.Errorf("error in creating cipher: %v", err)
-	}
+	//block, err := des.NewCipher(key)
+	//if err != nil {
+	//	return nil, fmt.Errorf("error in creating cipher: %v", err)
+	//}
+	//
+	//plaintext := make([]byte, len(ciphertext))
+	//blockMode := cipher.NewCBCDecrypter(block, iv)
+	//blockMode.CryptBlocks(plaintext, ciphertext)
 
-	plaintext := make([]byte, len(ciphertext))
-	blockMode := cipher.NewCBCDecrypter(block, iv)
-	blockMode.CryptBlocks(plaintext, ciphertext)
-
+	plaintext := Cipher(ciphertext, key, DECRYPT_MODE)
 	return plaintext, nil
 }
 
